7a9aabe05b0f68251fffdc0fdc8133b68cf00edc
Denzel <denzelsamala@Denzel-2.local>
2022-03-03 21:40:43 -0800

    group code front end auth w/ jwt

---
+++ client/src/components/Login/index.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import * as authService from "../../api/auth.service"
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
+    
+    const handleSubmit = async(e) => {
+        e.preventDevault();
+        await authService.login(email, password).then(() => {
+            setEmail=("");
+            setPassword=("");
+
+        })
+        setSuccessMsg("Hallelujah it works bruh");
+    }
+
+
+  return (
+    <div>
+        <form>
+            <label htmlFor="email">
+                Email:
+                <input
+                    onChange={(e) => setEmail(e.target.value)} 
+                    value={email}
+                    type="text"
+                    name="email"
+                    placeholder="Enter Your Email"
+                />
+            </label>
+            <label>
+                Enter Your Password:
+                <input 
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type="text"
+                    name="password"
+                    placeholder="Enter Your Password"
+                />
+            </label>
+            <button onClick={handleSubmit}>Sign In</button>
+            <h1>{successMsg}</h1>
+        </form>
+    </div>
+  )
+}
+
+export default Login
--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import Post from "../../components/Post";
 import User from "../../components/User";
 import PostForm from "../../components/PostForm";
 import NavBar from "../../components/NavBar";
 import Privacy from "../../components/Privacy";
+import Login from "../../components/Login";
 import LogOut from "../../components/LogOut";
 import * as postService from "../../api/post.service";
 import * as userService from "../../api/user.service";
+import * as authService from "../../api/auth.service";
 import { Routes, Route } from "react-router-dom";
+
 
 
 // take all of the posts from the db
@@ -15,17 +18,49 @@ import { Routes, Route } from "react-router-dom";
 // make sure new posts render when created
 // usestate, and useeffect for our posts, then map it out
 
-const Home = () => {
-	const [posts, setPosts] = useState([]);
+const reducer = (prevState, action) => {
+	switch(action.type) {
+		case "setPosts":
+			return{...prevState, posts:action.payload}
+		case "isLoggedIn":
+			return {...prevState, isLoggedIn: action.payload}
+		default:
+			return prevState;
+	}
+}
 
-	const fetchPosts = async () => {
+const initialState = {
+	posts: [],
+	isLoggedIn: false,
+}
+
+
+
+const Home = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const {posts, isLoggedIn} = state;
+	const [users, setUsers] = useState([]);
+
+	// const fetchPosts = async () => {
+	// 	await postService.getAllPost().then((res) => {
+	// 		console.log(res);
+	// 		setPosts(res.data.data.reverse());
+	// 	});
+	// };
+
+		const fetchPosts = async () => {
 		await postService.getAllPost().then((res) => {
-			console.log(res);
-			setPosts(res.data.data.reverse());
+				dispatch({ type: "setPosts", payload: res.data.data.reverse()});
 		});
 	};
 
-    const [users, setUsers] = useState([]);
+		const userActive = () => {
+			if(authService.currentUser()){
+				dispatch({ type: "isLoggedIn", payload: true})
+			} else
+				dispatch({ type: "isLoggedIn", payload: false})
+		}
+  
 
 	const fetchUsers = async () => {
 		await userService.getAllUser().then((res) => {
@@ -37,31 +72,33 @@ const Home = () => {
 	useEffect(() => {
 		fetchPosts();
         fetchUsers();
+		userActive();
 	}, []);
 
 
+if(isLoggedIn) {
 
-	return (
+return (	
         <div>
             <NavBar />
-<Routes>
+				<Routes>
 
-<Route path="/" element={
-	<>
-            <PostForm refreshPosts={() => fetchPosts()}/>
-            {posts.map((post) => {
-					return (
-			    <Post
-                title={post.title}
-                body={post.body}
-                comment = {post.comment}
-                image={post.image}
-                key={[post._id]} 
-				/>
-                )
-                })}
-	</>
-}
+				<Route path="/" element={
+					<>
+						<PostForm refreshPosts={() => fetchPosts()}/>
+						{posts.map((post) => {
+								return (
+							<Post
+							title={post.title}
+							body={post.body}
+							comment = {post.comment}
+							image={post.image}
+							key={[post._id]} 
+							/>
+							)
+							})}
+				</>
+			}
 ></Route>
 
 <Route path="users" element={
@@ -85,8 +122,18 @@ const Home = () => {
     <Route path="logout" element={<LogOut />}></Route>
 
  </Routes>
+	
+ 
         </div> 
-    )};	
-
+    )} 
+	
+		else {
+			return (
+				<div>
+					<Login />
+				</div>
+			)
+		};	
+};
 	
 export default Home;
