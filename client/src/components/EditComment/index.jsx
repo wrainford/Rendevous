4e48da8cacdcb0a9e9626a4c4d316b3fb9bc3a07
PredatorR10 <godofgamingpr10@gmail.com>
2022-03-05 21:22:03 -0500

    edit functionality finished

--- client/src/components/EditComment/index.jsx
+++ client/src/components/EditComment/index.jsx
@@ -6,12 +6,17 @@ const EditComment = (props) => {
     const [child, setChild] = useState("");
     const [button, setButton] = useState("Edit");
 
+    const Submit = () => {
+        setChild("");
+        setButton("Edit");
+    }
+
     const HandleClick = () => {
         if(child) {
             setChild("");
             setButton("Edit");
         } else {
-            setChild(<EditCommentForm body={props.body} />);
+            setChild(<EditCommentForm body={props.body} commentId={props.commentId} submit={Submit}/>);
             setButton("Cancel");
         }
     }
--- client/src/components/EditCommentForm/index.jsx
+++ client/src/components/EditCommentForm/index.jsx
@@ -1,19 +1,27 @@
-import React from "react";
-import * as PostService from "../../api/post.service"
+import React, { useState } from "react";
+import * as postService from "../../api/post.service"
 
 const EditCommentForm = (props) => {
-    const HandleSubmit = () => {
+    const [content, setContent] = useState(props.body);
 
+    const HandleSubmit = async () => {
+        let updatedComment = { content }
+        let res = await postService.updateComment(props.commentId, updatedComment).then(() => {
+            props.submit();
+        })
+
+        console.log(res);
     }
 
     return(
         <>
             <form>
                 <input
+                    onChange={(e) => setContent(e.target.value)}
                     type="text"
-                    value={props.body}
+                    value={content}
                 />
-                <button onclick={HandleSubmit}>Submit</button>
+                <button onClick={HandleSubmit}>Submit</button>
             </form>
         </>
     )
