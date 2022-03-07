412ea294586a0fbfd7b61408038977fbfbd10839
skim121 <skim29121@gmail.com>
2022-03-06 19:11:02 -0600

    modal is live

--- client/src/components/PostTest/index.jsx
+++ client/src/components/PostTest/index.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import * as postService from "../../api/post.service";
+import Modal from "react-modal"
 import Post from "../../components/Post";
 import PostEdit from "../../components/PostEdit";
 import "./index.css";
 import { FiEdit } from "react-icons/fi";
 import { FiTrash2 } from "react-icons/fi";
 import {GiCancel} from "react-icons/gi";
+Modal.setAppElement("#root");
+
 
 const PostView = (props) => {
     if (props.isEdit) {
@@ -52,6 +55,7 @@ const PostToggle = (props) => {
 
     const [isEdit, setIsEdit] = useState(false);
     const [button, setButton] = useState(<FiEdit size={30} color="#66AFA4"/>)
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     const handleClick = () => {
         console.log(isEdit)
@@ -59,18 +63,61 @@ const PostToggle = (props) => {
         { !isEdit ? (setButton(<GiCancel size={30} color="#66AFA4"/> )) : (setButton(<FiEdit size={30} color="#66AFA4"/>))}
     }
 
+    const handleModal = () => {
+       props.deletePosts(); 
+       setModalIsOpen(false);
+    }
+
     return(
         <div className="post-container">
-            
             <br />
             <div className="post-buttons">
             <button onClick={handleClick} className="button1">
                 {button}
             </button>
             &nbsp;&nbsp;
-            <button onClick ={props.deletePosts} className="button-trash">
+            <button 
+                className="openModalBtn" 
+                onClick={()=>setModalIsOpen(true)}> 
                 <FiTrash2 size={30} color="#66AFA4"/>
             </button>
+            <Modal 
+                isOpen={modalIsOpen} 
+                onRequestClose={()=>setModalIsOpen(false)}
+                style={
+                    { 
+                        overlay:{
+                            backgroundColor: 'rgba(255, 255, 255, 0.25)',
+                        },
+                        content:{
+                            backgroundColor: "#0D192D",
+                            verticalAlign:"center",
+                            color: "white",
+                            height: "200px",
+                            width: "50%",
+                            justifyContent: "center",
+                            textAlign: "center",
+                            top:"30%",
+                            left: "25%",
+                            right: "25%",
+                            bottom: "30%",
+                            fontFamily: 'Jost',
+                            border: "none"
+                        }
+                    }
+                }
+                >
+                <h2 className="modal-text">ARE YOU SURE YOU WANT TO DELETE?</h2>
+                <div>
+                    <button className="modal-close" onClick={()=>setModalIsOpen(false)}>Close</button>
+                    &nbsp;&nbsp;
+                    <button className="modal-delete" onClick={handleModal}>Delete</button>
+                    
+                </div>
+
+            </Modal>
+            
+
             </div>
             <PostView 
                 isEdit={isEdit}
