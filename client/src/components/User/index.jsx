5e3e9741418a560808d56d92ff11674ca582c892
Denzel <denzelsamala@Denzel-2.local>
2022-03-06 20:37:10 -0800

    user posts still getting undefined error as you move between pages. handing off to other team member

--- client/src/components/User/index.jsx
+++ client/src/components/User/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import './index.css';
+import PostTest from '../../components/PostTest';
 
 const User = (props) => {
+	const [posts, setPosts] = useState([]);
+	
+	useEffect(() =>{
+		setPosts(props.user.post);
+	}, []);
+	
 	return (
 		<>
 			<div className="prof-container">
-			<img src = {`/uploads/postImages/${props.props.avatar}`} alt="..." style= {{width: "80%"}}/>
-			<h1 className="prof-username">{props.props.userName}</h1>
-				<h2 className="prof-name">{props.props.name}</h2>
+			<img src = {`/uploads/postImages/${props.user.avatar}`} alt="..." style= {{width: "25%"}}/>
+			<h1 className="prof-username">{props.user.userName}</h1>
+				<h2 className="prof-name">{props.user.name}</h2>
 			</div>
+					{/* {posts.map((post) => {
+						return(
+							<PostTest 
+							title={post.title}
+							body={post.body}
+							comment = {post.comment}
+							image={post.image}
+							postId={post._id} 
+							/>
+						)
+			})} */}
 		</>
 	);
 }
