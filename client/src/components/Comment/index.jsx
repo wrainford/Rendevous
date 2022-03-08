75b70aa07b6563d8b182bfd4850d6a5447b6a799
Denzel <denzelsamala@Denzel-2.local>
2022-03-08 00:43:32 -0800

    some code housekeeping

--- client/src/components/Comment/index.jsx
+++ client/src/components/Comment/index.jsx
@@ -1,11 +1,10 @@
 // Post Component
 import React, { useEffect, useState } from "react";
-import * as postService from "../../api/post.service";
 import * as userService from "../../api/user.service";
 import DelComment from "../DeleteComment";
 import EditComment from "../EditComment";
 import "./index.css"
-import {BsPersonCircle} from "react-icons/bs"
+
 
 
 const Comment = (props) => {
@@ -23,9 +22,6 @@ const Comment = (props) => {
 		userQuery(props.userName);
 	},[])
 
-
-	//console.log(commentName)
-
 	return (
 		<>
 			<div className="comment-container">
@@ -39,31 +35,9 @@ const Comment = (props) => {
 				<EditComment commentId={props.id} body={props.comment}/>
 			</div>
 			<div className="delete-btn-div">
-			<DelComment commentId={props.id} refreshPosts={props.refreshPosts} className="delete-btn"/></div>
-
-
-
-
+				<DelComment commentId={props.id} refreshPosts={props.refreshPosts} className="delete-btn"/></div>
 
 			</div>
-
-		
-		{/* <div className="com-div">
-			<div className="grid1">
-				<BsPersonCircle size={30} />
-			</div>
-
-			<div className="comment-container">
-				<div className="com-edit-side">
-				
-				<h4 className="h4-comment-text"> {props.comment} </h4>
-				</div>
-			</div>
-			<div className="comment-buttons">
-				<EditComment commentId={props.id} body={props.comment}/>
-				<DelComment commentId={props.id} refreshPosts={props.refreshPosts}/>
-			</div>
-		</div> */}
 		</>
 	);
 };
