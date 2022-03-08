29c37b38d852e64f5c8e866c23147d1101db1af6
Denzel <denzelsamala@Denzel-2.local>
2022-03-08 01:22:22 -0800

    last edit before deployment

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const backendAPI = "http://localhost:4000/api";
-// const backendAPI = "https://rendevousss.herokuapp.com/api";
+// const backendAPI = "http://localhost:4000/api";
+const backendAPI = "https://rendevousss.herokuapp.com/api";
 
 const user = JSON.parse(localStorage.getItem("user"));
 const apiClient = axios.create({
